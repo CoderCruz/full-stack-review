d7607e49f34e8b0148480253b0da26fd709df868
Jose Cruz <cruzjosework@gmail.com>
2024-08-01 14:52:48 -0400

    added delete functionality

--- recipes/frontend/food-recipes/src/pages/DeleteRecipe.jsx
+++ recipes/frontend/food-recipes/src/pages/DeleteRecipe.jsx
@@ -1,6 +1,32 @@
+import {useState, useEffect} from 'react'
+import Spinner from "../components/Spinner"
+import { deleteRecipeData } from '../services/getRecipes'
+import { useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+
 const DeleteRecipe = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false)
+  const { id } = useParams()
+
+  useEffect(()=> {
+    handleUpdateRecipe()
+  }, [])
+
+  const handleUpdateRecipe = () => {
+    setLoading(true)
+    deleteRecipeData(id).then(() => {
+      setLoading(false)
+      navigate('/');
+      window.location.reload();
+    })
+  }
+
+
   return (
-    <div>DeleteRecipe</div>
+    <div>
+      {loading ? <Spinner /> : <div>Delete Recipe</div>}
+    </div>
   )
 }
 
